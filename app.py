853bcd42e821c02ae1e0911d786cec9d14ab30f0
canvasseremily6 <canvasseremily6@gmail.com>
2026-08-15 21:08:18 -0600

    Typos in app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import os
 
-API = os.environ.get('API','http://127.0.0.1:8000')
+API = os.environ.get('API_URL','http://127.0.0.1:8000')
 
 st.title('Book Recommendations System')
 st.markdown('Tell me your favorite book and I will give you two recommendations!')
